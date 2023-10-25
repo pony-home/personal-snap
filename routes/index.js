4e0cff02a54d1f08803bb6d3121675c265bd07d5
Kristina Dmitrieva <kristin1kozlovskaya@gmail.com>
2023-10-25 13:59:38 +0200

    Add health markers section to doc + fitness to input form

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,8 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
 
+  const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in suscipit purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus nec hendrerit felis. Morbi aliquam facilisis risus eu lacinia. Sed eu leo in turpis fringilla hendrerit. Ut nec accumsan nisl. Suspendisse rhoncus nisl posuere tortor tempus et dapibus elit porta. Cras leo neque, elementum a rhoncus ut, vestibulum non nibh. Phasellus pretium justo turpis. Etiam vulputate, odio vitae tincidunt ultricies, eros odio dapibus nisi, ut tincidunt lacus arcu eu elit.';
+
   let fileName = 'Annual snapshot'
   res.setHeader('Content-disposition', 'inline; filename='+ fileName +'.pdf');
 
@@ -28,7 +30,7 @@ router.post('/', function(req, res, next) {
   doc.fontSize(20).fillColor('gray').text("KRISTINA, 31 years", 210, currentY).fillOpacity(1);
 
   currentY = 75;
-  doc.roundedRect(40, currentY-5, 250, 170, 30).fillOpacity(0.3).fillAndStroke("gray", "gray");
+  doc.roundedRect(40, currentY-5, 250, 170, 20).fillOpacity(0.3).fill("gray");
   doc.fillOpacity(1).image("public/images/image.jpeg", 60, currentY, {height: 160,  align: 'center', valign: 'center'});
 
 
@@ -153,10 +155,10 @@ router.post('/', function(req, res, next) {
 // HEALTH SECTION - START
   currentY = 250;
   doc.rect(0, currentY, titleBoxWidth, titleBoxHeight)
-    .fillOpacity(0.6)
+    .fillOpacity(0.4)
     .fill("gray");
 
-  doc.fontSize(12).fillColor('black').text("PHYSICAL HEALTH AND FITNESS", 200, currentY + 10);
+  doc.fontSize(12).fillOpacity(1).fillColor('white').text("PHYSICAL HEALTH AND FITNESS", 200, currentY + 10);
 
   currentY = 290;
   // AESTHETIC PART
@@ -164,33 +166,72 @@ router.post('/', function(req, res, next) {
   //   .stroke();
 
   if (req.body.genderRadio === 'Female') {
-    doc.fontSize(12).fillColor('black').text("32", 200, currentY+20);
+    // doc.fontSize(12).fillColor('black').text("32", 200, currentY+20);
 
       doc.image("public/images/female.jpg", 50, currentY, {height: 240,  align: 'center', valign: 'center'});
   } else {  
     doc.image("public/images/male.jpg", 50, currentY, {height: 240,  align: 'center', valign: 'center'});
     // doc.fontSize(10).fillColor('black').text("32", 120, currentY+50);
+    // TODO: add BMI scale on the bottom
 
   }
 
 
 
   // HEALTH MARKERS PART
-  doc.rect(240, currentY, 160, 120)
-    .stroke();
+  let physicalBoxWidth = 170;
 
-  // FUNCTIONAL PART
-  doc.rect(420, currentY, 160, 120)
-    .stroke();
+  doc.strokeOpacity(0.6);
+  // doc.rect(220, currentY, physicalBoxWidth, 120).stroke();
+  let markersRectWidth = 70;
+  let markersValueRectWidth = 90;
+
+  doc.fontSize(10).fillColor("gray").text("Health markers", 225, currentY +5);
+  doc.rect(225, currentY+20, markersRectWidth, 12).fillAndStroke("gray", "white");
+  doc.rect(225, currentY+32, markersRectWidth, 12).fillAndStroke("gray", "white");
+  doc.rect(225, currentY+44, markersRectWidth, 12).fillAndStroke("gray", "white");
+  doc.rect(225, currentY+56, markersRectWidth, 12).fillAndStroke("gray", "white");
+  doc.rect(225, currentY+68, markersRectWidth, 12).fillAndStroke("gray", "white");
+  doc.rect(225, currentY+80, markersRectWidth, 12).fillAndStroke("gray", "white");
+
+  doc.fillOpacity(0.2).rect(295, currentY+20, markersValueRectWidth, 12).fillAndStroke("gray", "white");
+  doc.rect(295, currentY+32, markersValueRectWidth, 12).fillAndStroke("gray", "white");
+  doc.rect(295, currentY+44, markersValueRectWidth, 12).fillAndStroke("gray", "white");
+  doc.rect(295, currentY+56, markersValueRectWidth, 12).fillAndStroke("gray", "white");
+  doc.rect(295, currentY+68, markersValueRectWidth, 12).fillAndStroke("gray", "white");
+  doc.rect(295, currentY+80, markersValueRectWidth, 12).fillAndStroke("gray", "white");
+
+  // TODO: add units of measurments
+  // TODO: add green,yellow, red background color, depending on the value (or dor after the value instead of background)
+  doc.fontSize(7).fillColor("white").fillOpacity(1).text("Blood pressure", 230, currentY+23);
+  doc.fillColor("white").text("Blood cholesterol", 230, currentY+35);
+  doc.fillColor("white").text("Blood glucose", 230, currentY+47);
+  doc.fillColor("white").text("Body fat", 230, currentY+59);
+  doc.fillColor("white").text("VO2max", 230, currentY+71);
+  doc.fillColor("white").text("Resting heart rate", 230, currentY+83);
+
+  // TODO: add values from input form
+  doc.fontSize(7).fillColor("gray").fillOpacity(1).text("120/80 mmHg", 300, currentY+23);
+  doc.text("4 mmol/L", 300, currentY+35);
+  doc.text("4 mmol/L", 300, currentY+47);
+  doc.text("26% (DEXA scan)", 300, currentY+59);
+  doc.text("50 ml/kg/min", 300, currentY+71);
+  doc.text("40 bpm", 300, currentY+83);
+
+
+  // OTHERS PART
+  // doc.strokeColor("gray").fillColor("gray").rect(400, currentY, physicalBoxWidth, 120).stroke();
+
 
   currentY = 420;
-  // REST PART
-  doc.rect(240, currentY, 160, 120)
-    .stroke();
+  // FITNESS PART
+  // doc.rect(220, currentY, physicalBoxWidth, 120).stroke();
 
   // FREE TEXT PART
-  doc.rect(420, currentY, 160, 120)
-    .stroke();
+  // doc.rect(400, currentY, physicalBoxWidth, 120).stroke();
+  doc.fontSize(10).fillColor("gray").text("Summary", 405, currentY +5);
+  doc.fontSize(7).fillColor("gray").text(lorem, 405, currentY+20, {align: 'justify', width: 160});
+
 
 // HEALTH SECTION - END
 
